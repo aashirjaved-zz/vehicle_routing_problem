c03887dc0a34b42592dfda5190ac1d4056ca8ff5
aashirjaved <aashir.javed36@ce.ceme.edu.pk>
2019-02-15 12:05:07 +0500

    removed or tools

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ const dataSet = {
             "Long": 39.0
         },
         {
-            "PassengerId":5,
+            "PassengerId": 5,
             "Lat": 21.22,
-            "Long":40.3
+            "Long": 40.3
         }
 
     ],
@@ -83,45 +83,33 @@ var node_or_tools = require('node_or_tools')
 //Allotting stations to passengers 
 employeeAllotedStation = [];
 for (employee in dataSet.Passengers) {
-    let smallestDistance = {stationId:"None",distance:dataSet.MaxDistanceFromStation};
+    let smallestDistance = {
+        stationId: "None",
+        distance: dataSet.MaxDistanceFromStation
+    };
     for (stations in dataSet.Stations) {
         let employePoint = new geoPoint(dataSet.Passengers[employee].Lat, dataSet.Passengers[employee].Long);
         let stationPoint = new geoPoint(dataSet.Stations[stations].Lat, dataSet.Stations[stations].Long)
         var distanceFromStation = employePoint.distanceTo(stationPoint, true)
-        if(smallestDistance.distance>distanceFromStation){
-            smallestDistance.stationId =  dataSet.Stations[stations].StationId;
+        if (smallestDistance.distance > distanceFromStation) {
+            smallestDistance.stationId = dataSet.Stations[stations].StationId;
             smallestDistance.distance = distanceFromStation
         }
     }
     employeeAllotedStation.push({
-        passengerId:dataSet.Passengers[employee].PassengerId,
-        stationAlloted : smallestDistance.stationId,
-        distance : smallestDistance.distance
+        passengerId: dataSet.Passengers[employee].PassengerId,
+        stationAlloted: smallestDistance.stationId,
+        distance: smallestDistance.distance
     })
 }
 console.log(employeeAllotedStation)
 
-//Finding distances between stations
-var vrpSolverOpts = {
-    numNodes: 3,
-    costs: [[0, 10, 10], [10, 0, 10], [10, 10, 0]],
-    durations: [[0, 2, 2], [2, 0, 2], [2, 2, 0]],
-    timeWindows: [[0, 9], [2, 3], [2, 3]],
-    demands: [[0, 0, 0], [1, 1, 1], [1, 1, 1]]
-  };
 
-  var vrpSearchOpts = {
-    computeTimeLimit: 1000,
-    numVehicles: 3,
-    depotNode: 0,
-    timeHorizon: 9 * 60 * 60,
-    vehicleCapacity: 3,
-    routeLocks: [[], [], []],
-    pickups: [4, 9],
-    deliveries: [12, 8]
-  };
-  
-  var VRP = new node_or_tools.VRP(vrpSolverOpts);
-  VRP.Solve(vrpSearchOpts,function(err,success){
-      console.log(err,success)
-  })
+//finding nearby stations 
+for (stations in dataSet.Stations) {
+    let station1Point = new geoPoint(dataSet.Stations[stations].Lat, dataSet.Stations[stations].Long);
+    for (allStations in dataSet.Stations) {
+        let newStationPoint = new geoPoint(dataSet.Stations[allStations].Lat, dataSet.Stations[allStations].Long); 
+        var distanceBetweenStations = station1Point.distanceTo(newStationPoint, true)
+    }
+}
