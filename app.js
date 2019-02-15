301a0aa1ded0e6b0efe82679d7038c7d3270deea
aashirjaved <aashir.javed36@ce.ceme.edu.pk>
2019-02-15 17:11:56 +0500

    added stations distances

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 const dataSet = {
     "Passengers": [{
             "PassengerId": 1,
-            "Lat": 31.0012,
-            "Long": 32.12
+            "Lat": 33.664826,
+            "Long": 73.065371
         },
         {
             "PassengerId": 2,
-            "Lat": 31.99,
-            "Long": 32.231
+            "Lat": 33.670181,
+            "Long": 73.063589
         },
         {
             "PassengerId": 3,
-            "Lat": 39.11,
-            "Long": 31.01
+            "Lat": 33.663152,
+            "Long": 73.076959
         },
         {
             "PassengerId": 4,
-            "Lat": 30.95,
-            "Long": 39.0
+            "Lat": 33.669514,
+            "Long":72.989343
         },
         {
             "PassengerId": 5,
-            "Lat": 21.22,
-            "Long": 40.3
+            "Lat": 33.671604,
+            "Long": 72.992541
+        },
+        {
+            "PassengerId": 6,
+            "Lat": 33.129651,
+            "Long": 73.692124
+        },
+        {
+            "PassengerId": 7,
+            "Lat": 33.649913,
+            "Long": 73.043938
+        },
+        {
+            "PassengerId": 8,
+            "Lat": 33.645992,
+            "Long": 73.036316
+        },
+        {
+            "PassengerId": 9,
+            "Lat": 33.640917,
+            "Long": 73.025848
         }
 
     ],
@@ -30,23 +50,18 @@ const dataSet = {
     "ETA": "08:00",
     "Stations": [{
             "StationId": 1,
-            "Lat": 31.0012,
-            "Long": 32.12
+            "Lat": 33.668565,
+            "Long": 73.071782
         },
         {
             "StationId": 2,
-            "Lat": 31.99,
-            "Long": 32.231
+            "Lat": 33.681344,
+            "Long": 73.018948
         },
         {
             "StationId": 3,
-            "Lat": 39.99,
-            "Long": 31.231
-        },
-        {
-            "StationId": 4,
-            "Lat": 30.99,
-            "Long": 39.231
+            "Lat": 33.695300,
+            "Long":  72.979676
         }
     ],
     "MaxDistanceFromStation": "500.00",
@@ -80,6 +95,15 @@ var distance = require('google-distance');
 var node_or_tools = require('node_or_tools')
 
 
+
+function arePointsNear(checkPoint, centerPoint, km) {
+    var ky = 40000 / 360;
+    var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
+    var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
+    var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
+    return Math.sqrt(dx * dx + dy * dy) <= km;
+}
+
 //Allotting stations to passengers 
 employeeAllotedStation = [];
 for (employee in dataSet.Passengers) {
@@ -102,14 +126,22 @@ for (employee in dataSet.Passengers) {
         distance: smallestDistance.distance
     })
 }
-console.log(employeeAllotedStation)
+//console.log(employeeAllotedStation)
 
 
 //finding nearby stations 
 for (stations in dataSet.Stations) {
-    let station1Point = new geoPoint(dataSet.Stations[stations].Lat, dataSet.Stations[stations].Long);
+    var station1 = {lat: dataSet.Stations[stations].Lat, lng:dataSet.Stations[stations].Long}
     for (allStations in dataSet.Stations) {
-        let newStationPoint = new geoPoint(dataSet.Stations[allStations].Lat, dataSet.Stations[allStations].Long); 
-        var distanceBetweenStations = station1Point.distanceTo(newStationPoint, true)
+        if(dataSet.Stations[stations].StationId===dataSet.Stations[allStations].StationId){
+            //do nothing
+        } else{
+            var station2 =  {lat: dataSet.Stations[allStations].Lat, lng:dataSet.Stations[allStations].Long}
+            var n = arePointsNear(station1, station2, 5);
+            if(n){
+                console.log(dataSet.Stations[stations].StationId)
+                console.log(dataSet.Stations[allStations].StationId)
+            }
+        }
     }
 }
